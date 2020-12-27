f75478b348aee7bdd3029b161a98fb096272c6fc
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2020-12-27 16:31:08 +0700

    init
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

---
+++ Makefile
@@ -0,0 +1,17 @@
+PORT=8080
+CI_REGISTRY_IMAGE=jmalloc/echo-server:exp-feat-1ecfd275763eff1d6b4844ea3168962458c9f27a
+PROXY_STATUS=ext
+.PHONY: generate-yaml update-proxy deploy-container deploy-proxy delete-prev-container deploy
+
+generate-yaml:
+	python3 gen.py $(PREV_SHA) $(CURR_SHA) $(BRANCH_NAME) $(PORT) $(PROXY_STATUS)
+update-proxy: generate-yaml
+	docker exec envoy mv /var/lib/envoy/cds-new.yaml /var/lib/envoy/cds.yaml && mv /var/lib/envoy/lds-new.yaml /var/lib/envoy/lds.yaml
+deploy-proxy: generate-yaml
+	docker run --name envoy -d -p 80:10000 -p 9901:9901 -v ${PWD}/envoy.yaml:/etc/envoy/envoy.yaml -v ${PWD}/yaml:/var/lib/envoy --network envoy envoyproxy/envoy-alpine:v1.16.0
+deploy-container:
+	docker run --name $(BRANCH_NAME)-$(CURR_SHA) --hostname $(BRANCH_NAME)-$(CURR_SHA) -d --network envoy $(CI_REGISTRY_IMAGE)
+delete-prev-container:
+	docker container rm -f $(BRANCH_NAME)-$(PREV_SHA)
+
+deploy: deploy-container generate-yaml deploy-proxy delete-prev-container
---
+++ gen.py
@@ -0,0 +1,56 @@
+import sys
+import jinja2
+from ruamel.yaml import YAML
+yaml = YAML(typ='safe')
+
+## example command 
+## python3 gen.py 0000000000000000000000000000000000000000 1ecfd275763eff1d6b4844ea3168962458c9f27a fitur-1
+## in order $CI_COMMIT_BEFORE_SHA $CI_COMMIT_SHA $CI_COMMIT_REF_SLUG  
+DOMAIN = "2pai.local"
+
+## vars as defined below
+prev_sha = sys.argv[1]
+curr_sha = sys.argv[2]
+curr_branch = sys.argv[3]
+svc_port = sys.argv[4]
+status = sys.argv[5]
+
+def value_yaml(prev,curr,branch,port):
+    value_yaml = open('vars/value.yaml').read()
+    val = yaml.load(value_yaml)
+
+    # check if it's the new MR x branch & append init data
+    # new MR in some branch always have prev_sha with 0000000000000000000000000000000000000000
+    if prev == "0000000000000000000000000000000000000000":
+        val['list_svc'].append({
+            'cluster_name': branch + "-cluster",
+            'service_name': branch + "-"+ curr, 
+            'service_port': int(port),    
+            'domain': [ branch+'-*.'+DOMAIN]
+        })
+    for data in val['list_svc']:
+        if data['cluster_name'] == branch+"-cluster" and data['service_name'] == (branch + "-"+ prev):
+            data['service_name'] = branch + "-"+ curr
+
+    with open('vars/value.yaml','w') as out_yaml:
+        yaml.dump(val,out_yaml) 
+
+    return val
+
+# generate eds & lds function based on value.yaml & args
+def generate_config(prev,curr,branch,svc_port):
+    val = value_yaml(prev,curr,branch,svc_port)
+
+    eds_template = open('templates/cds.yaml.j2').read()
+    lds_template = open('templates/lds.yaml.j2').read()
+    eds_config = jinja2.Template(eds_template).render(val)
+    lds_config = jinja2.Template(lds_template).render(val)
+    if status == 'new':
+        open('yaml/cds.yaml', 'w').write(eds_config)
+        open('yaml/lds.yaml', 'w').write(lds_config)
+    else:
+        open('yaml/cds-new.yaml', 'w').write(eds_config)
+        open('yaml/lds-new.yaml', 'w').write(lds_config)
+        
+    print("Successfully Generate YAML for envoy-proxy")
+generate_config(prev_sha,curr_sha,curr_branch,svc_port)
